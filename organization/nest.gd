603ecf16ec70b3a94a15017ed7d1e4f9f01bdb30
DoctorTangerina <arthursardellajiujitsu@gmail.com>
2026-07-15 14:53:15 -0300

    04-nest implemented

--- organization/nest.gd
+++ organization/nest.gd
@@ -1,4 +1,17 @@
 extends Node2D
 
-func _ready() -> void:
-	pass
+var food_storage: int = 0
+var wood_storage: int = 0
+
+
+func deposit(resource_type: String, amount: int) -> void:
+	if resource_type == "Food":
+		food_storage += amount
+		print("Nest: deposited ", amount, " Food. Total: ", food_storage)
+	elif resource_type == "Wood":
+		wood_storage += amount
+		print("Nest: deposited ", amount, " Wood. Total: ", wood_storage)
+
+
+func get_storage_summary() -> Dictionary:
+	return {"Food": food_storage, "Wood": wood_storage}
